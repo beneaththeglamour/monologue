6f7cc79ccc018fec5ee6c70043fa945e2a2b99b4
aixxe <me@aixxe.net>
2016-10-11 04:23:07 +0100

    Footer now uses random quotes and the actual blog title.

--- resources/views/layouts/templates/footer.blade.php
+++ resources/views/layouts/templates/footer.blade.php
@@ -1,3 +1,14 @@
+<?php
+
+	// Random quotes shown in the footer title.
+	$quotes = [
+		['This is a quote that will appear in the footer.', 'Some guy']
+	];
+
+	$random_quote = $quotes[array_rand($quotes)];
+
+?>
+
 @section('footer')
 	<footer>
 		<div class="container">
@@ -11,27 +22,31 @@
 
 				<section class="col-lg-9 body">
 					<div class="title">
-						designed &amp; developed by aixxe
+						&ldquo;{{ $random_quote[0] }}&rdquo;
+						
+						<small class="author">
+							&mdash; {{ $random_quote[1] }}
+						</small>
 					</div>
 
 					<p class="about">
-						My blog where I publish a variety of interesting things, mostly related to development and technology with some guest appearences by friends.
+						Here's some information about the blog. Well, not really. You have to add some stuff here yourself.
 					</p>
 
 					<p class="copyright">
-						&copy; {{ date("Y") }}, aixxe.net &mdash; <a href="https://github.com/aixxe/monologue">powered by monologue</a>
+						&copy; {{ date("Y") }}, {{ env('BLOG_TITLE') }} &mdash; <a href="https://github.com/aixxe/monologue">powered by monologue</a>
 					</p>
 
 					<p class="social">
-						<a target="_blank" title="Twitter" href="https://twitter.com/a1xxe">
+						<a target="_blank" title="Twitter" href="#">
 							<span class="icon-twitter-square"></span>
 						</a>
 
-						<a target="_blank" title="GitHub" href="https://github.com/aixxe">
+						<a target="_blank" title="GitHub" href="#">
 							<span class="icon-github-square"></span>
 						</a>
 						
-						<a target="_blank" title="Steam" href="https://steamcommunity.com/id/aixxe">
+						<a target="_blank" title="Steam" href="#">
 							<span class="icon-steam-square"></span>
 						</a>
 					</p>
