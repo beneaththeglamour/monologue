e05805ca43a91c9becc97e6b9ae6e772785acb30
aixxe <me@aixxe.net>
2016-10-15 07:19:22 +0100

    Implemented the appropriate styles for the login page.

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -2,59 +2,59 @@
 
 @section('content')
 <div class="container auth">
-    <div class="col-md-6 col-md-offset-3">
-        <div class="auth-title">
+    <div class="col-lg-8 offset-lg-2">
+        <div class="title">
             <span>Sign in</span>
             
             @if (starts_with(Request::url(), 'https')) 
-            <div class="auth-subtitle">
+            <div class="subtitle">
                 <span class="icon-locked icon-medium"></span> Your connection is secure.
             </div>
             @endif
         </div>
         
-        <form class="form-horizontal" role="form" method="POST" action="{{ url('/login') }}">
+        <form role="form" method="POST" action="{{ action('Auth\LoginController@login') }}">
             {!! csrf_field() !!}
             
-            <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                <label class="col-md-4 control-label auth-control">Email address</label>
+            <div class="form-group row{{ $errors->has('email') ? ' has-danger' : '' }}">
+                <label class="col-lg-3 control col-form-label">Email address</label>
 
-                <div class="col-md-8">
-                    <input type="email" class="form-control auth-control" name="email" value="{{ old('email') }}">
+                <div class="col-lg-9">
+                    <input type="email" class="form-control control" name="email" value="{{ old('email') }}">
                     
                     @if ($errors->has('email'))
-                        <small class="help-block">
+                        <small class="form-control-feedback">
                             {{ $errors->first('email') }}
                         </small>
                     @endif
                 </div>
             </div>
 
-            <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                <label class="col-md-4 control-label auth-control">Password</label>
+            <div class="form-group row{{ $errors->has('password') ? ' has-danger' : '' }}">
+                <label class="col-lg-3 control">Password</label>
 
-                <div class="col-md-8">
-                    <input type="password" class="form-control auth-control" name="password">
+                <div class="col-lg-9">
+                    <input type="password" class="form-control control" name="password">
                     
                     @if ($errors->has('password'))
-                        <small class="help-block">
+                        <small class="form-control-feedback">
                             {{ $errors->first('password') }}
                         </small>
                     @endif
                 </div>
             </div>
             
-            <div class="form-group">
-                <div class="col-md-8 col-md-offset-4">
-                    <div class="pull-left">
-                        <div class="checkbox auth-checkbox">
-                            <label>
-                                <input type="checkbox" name="remember"> <span>Remember me</span>
+            <div class="form-group row">
+                <div class="col-lg-9 offset-lg-3">
+                    <div class="pull-xs-left">
+                        <div class="form-check checkbox">
+                            <label class="form-check-label">
+                                <input type="checkbox" class="form-check-input" name="remember"> Remember me
                             </label>
                         </div>
                     </div>
                     
-                    <div class="pull-right">
+                    <div class="pull-xs-right">
                         <button type="submit" class="btn">
                             Login
                         </button>
