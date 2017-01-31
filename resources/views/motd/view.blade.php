ebf29b591164db4a24d2e343de634d51a62bbb08
aixxe <me@aixxe.net>
2017-01-31 01:27:39 +0000

    Fix logo not appearing in MOTD pages.
    Signed-off-by: aixxe <me@aixxe.net>
    (cherry picked from commit 37e9bc8e25e8038417d42ae49024fdecf110941b)

--- resources/views/motd/view.blade.php
+++ resources/views/motd/view.blade.php
@@ -40,72 +40,76 @@
 
 <!DOCTYPE html>
 <html lang="{{ env('BLOG_LANGUAGE') }}">
-    @yield('headerinclude')
-    <body>
-    	<main>
-	        <header class="showcase motd" id="banner">
-	        	<div class="overlay">
-	        		<div class="container">
-	        			<div class="row">
-	        				<div class="col-xs-12 col-lg-6">
-	        		            <div class="logo" id="logo-alt">
-	        	            	    <a href="{{ env('APP_URL') }}"></a>
-	        	            	</div>
-	        		        </div>
-	        		        
-	        		        <div class="col-xs-12 col-lg-6">
-	        		            <nav>
-	        		                @yield('navbar')
-	        		            </nav>
-	        		        </div>
-	        			</div>
+	@yield('headerinclude')
+	<body>
+		<main>
+			<header class="showcase motd" id="banner">
+				<div class="overlay">
+					<div class="container">
+						<div class="row">
+							<div class="col-xs-12 col-lg-6">
+								<div class="logo" id="logo-alt">
+								   <a href="{{ action('BlogController@index') }}">
+										<svg xmlns="http://www.w3.org/2000/svg" xml:space="preserve" viewBox="0 0 200 29" version="1.1" style="fill: white;">
+											{!! file_get_contents('../resources/assets/img/logo.svg') !!}
+										</svg>
+									</a>
+								</div>
+							</div>
+							
+							<div class="col-xs-12 col-lg-6">
+								<nav>
+									@yield('navbar')
+								</nav>
+							</div>
+						</div>
 
-	        			<div class="content">
-				        	<section>
-				        		@if ($message->youtube)
-				        			<div class="embed embed-responsive embed-responsive-16by9">
-			        					<iframe class="embed-responsive-item" src="//youtube.com/embed/{{ $message->youtube }}" allowfullscreen></iframe>
-			        				</div>
-				        		@endif
+						<div class="content">
+							<section>
+								@if ($message->youtube)
+									<div class="embed embed-responsive embed-responsive-16by9">
+										<iframe class="embed-responsive-item" src="//youtube.com/embed/{{ $message->youtube }}" allowfullscreen></iframe>
+									</div>
+								@endif
 
-				        		<div class="message">
-					        		<span class="header">MESSAGE OF THE DAY</span>
+								<div class="message">
+									<span class="header">MESSAGE OF THE DAY</span>
 
-					        		<div class="navigation">
-					        			@yield('motd-navigation')
-					        		</div>
-					        		
-				        			@if ($message->content)
-				        				<p class="italic">&ldquo;{!! $message->content !!}&rdquo;</p>
-				        			@elseif (!$message->youtube)
-				        				<p>Nothing here for this day.</p>
-				        			@endif
+									<div class="navigation">
+										@yield('motd-navigation')
+									</div>
+									
+									@if ($message->content)
+										<p class="italic">&ldquo;{!! $message->content !!}&rdquo;</p>
+									@elseif (!$message->youtube)
+										<p>Nothing here for this day.</p>
+									@endif
 
-				        			<div class="share">
-				        				<ul class="list-inline">
-				        					<li class="list-inline-item">
-				        						<a href="https://www.facebook.com/sharer/sharer.php?u={{ $message->permalink }}">
-				        							<span class="icon-facebook"></span>
-				        						</a>
-				        					</li>
+									<div class="share">
+										<ul class="list-inline">
+											<li class="list-inline-item">
+												<a href="https://www.facebook.com/sharer/sharer.php?u={{ $message->permalink }}">
+													<span class="icon-facebook"></span>
+												</a>
+											</li>
 
-				        					<li class="list-inline-item">
-				        						<a href="https://twitter.com/home?status={{ $title.' '.$message->permalink }}">
-				        							<span class="icon-twitter"></span>
-				        						</a>
-				        					</li>
-				        				</ul>
-				        			</div>
+											<li class="list-inline-item">
+												<a href="https://twitter.com/home?status={{ $title.' '.$message->permalink }}">
+													<span class="icon-twitter"></span>
+												</a>
+											</li>
+										</ul>
+									</div>
 
-				        			<span class="footer">
-				        				&mdash; {{ strtoupper(env('BLOG_TITLE')) }} &mdash;
-				        			</span>
-				        		</div>
-	    			        </section>
-    			        </div>
-	        		</div>
-	        	</div>
-	        </header>
-        </main>
-    </body>
+									<span class="footer">
+										&mdash; {{ strtoupper(env('BLOG_TITLE')) }} &mdash;
+									</span>
+								</div>
+							</section>
+						</div>
+					</div>
+				</div>
+			</header>
+		</main>
+	</body>
 </html>
